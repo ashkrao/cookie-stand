029126f6a3b08dfdfdc201f708c8bf29afa655b7
Ashwini Rao <ashkrao@gmail.com>
2017-06-14 22:19:27 -0700

    Added form, deleted comments

--- js/sales.js
+++ js/sales.js
@@ -44,38 +44,6 @@ var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
 var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
-/*
-var generateList = function (store) {
-
-  var parentElement = document.getElementById('stores');
-
-  var h2 = document.createElement('h2');
-  h2.textContent = store.name;
-  parentElement.appendChild(h2);
-
-  var ul = document.createElement('ul');
-  parentElement.appendChild(ul);
-
-  for (var i = 0; i < store.cookiesPurchasedEachHour.length; i++) {
-    var li = document.createElement('li');
-    var hour = i + 6;
-    var time = 'am';
-    if(hour > 12) {
-      hour = hour - 12;
-      time = 'pm';
-    }
-    li.textContent = hour + time + ': ' + store.cookiesPurchasedEachHour[i] + ' cookies';
-    ul.appendChild(li);
-  }
-};
-
-generateList(firstAndPike);
-generateList(seaTacAirport);
-generateList(seattleCenter);
-generateList(capitolHill);
-generateList(alki);
-*/
-
 var getColumnSum = function(i) {
   return firstAndPike.cookiesPurchasedEachHour[i] + seaTacAirport.cookiesPurchasedEachHour[i] + seattleCenter.cookiesPurchasedEachHour[i] + capitolHill.cookiesPurchasedEachHour[i] + alki.cookiesPurchasedEachHour[i];
 };
